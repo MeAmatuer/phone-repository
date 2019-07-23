51399344a284066b97a722a8b29373bebd2eff55
MeAmatuer <sandipan2602@gmail.com>
2019-07-23 20:49:30 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 function App() {
   return (
-    <div>      
+    <div>
+      {/* <div className="header">     
+          Phone Directory 
+      </div>
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+      
+      <br/>
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name"></label>
+      <input type="text" id="name" placeholder="Type here" />
     </div>
   );
 }
