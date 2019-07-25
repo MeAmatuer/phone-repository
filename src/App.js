624236326894117b32545d11682e1b8556307e24
MeAmatuer <sandipan2602@gmail.com>
2019-07-26 01:27:44 +0530

    Created 'Header' Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React,{Component}  from 'react';
+import Header from './Header';
 
-function App() {
-  return (
-    <div>
-      {/* <div className="header">     
-          Phone Directory 
-      </div>
-      <button>Add</button>
+class App extends Component {
+  render(){
+    return (
       <div>
-        <span>Name</span>
-      
-      <br/>
-        <span>Phone</span>
-      </div> */}
-      <label htmlFor="name"></label>
-      <input type="text" id="name" placeholder="Type here" />
-    </div>
-  );
+        <Header/>
+        <button>Add</button>
+        <div>
+          <span>Name</span>
+        
+        <br/>
+          <span>Phone</span>
+        </div>
+      </div>
+    );
+  };
+  
 }
 
 export default App;
