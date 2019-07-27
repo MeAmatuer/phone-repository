479678958162af79f69697757d3bd66dc9047cb6
MeAmatuer <sandipan2602@gmail.com>
2019-07-27 22:17:13 +0530

    Passed heading to Header component as props

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     ];
     return (
       <div>
-        <Header/>
+        <Header heading="Phone Directory"/>
         <button className="add-btn">Add</button>
         <div>
         <div className="column-heading">
