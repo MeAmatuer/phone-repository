3ece5493cca52cdc8d89cfe859e9ce3dfed5cccf
MeAmatuer <sandipan2602@gmail.com>
2019-07-27 20:42:59 +0530

    Rendered Subscribers Dynamically. Added 'Delete' button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React,{Component}  from 'react';
 import Header from './Header';
+import './App.css';
 
 class App extends Component {
   render(){
+    let subscribers = [
+      {
+        id: 1,
+        name: "Sandy",
+        phone: 6787888988
+      },
+      {
+        id: 2,
+        name: "Akshay",
+        phone: "8498989008"
+      }
+    ];
     return (
       <div>
         <Header/>
-        <button>Add</button>
+        <button className="add-btn">Add</button>
         <div>
-          <span>Name</span>
+        <div className="column-heading">
+          <span className="grid-item">Name</span>
         
         <br/>
-          <span>Phone</span>
+          <span className="grid-item">Phone</span>
+        </div>
+        {subscribers.map(function (subscriber) {
+          return <div className="column-heading" key={subscriber.id}>
+          <span className="grid-item">{subscriber.name}</span>
+        
+        <br/>
+          <span className="grid-item">{subscriber.phone}</span>
+          <span><button className="delete-btn">Delete</button></span>
+        </div>
+        })}
+
         </div>
       </div>
     );
